ec0db56395cc785fd5305445932f645cc1a6ee11
Rapeepat <frk24072561@gmail.com>
2026-09-14 13:36:47 +0700

    up

---
+++ resources/views/index.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('title', 'หน้าแรก')
+
+@section('content')
+    <h2>ยินดีต้อนรับเข้าสู่เว็บไซต์ของฉัน My App 2</h2>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatem eveniet, quis odit architecto illum
+        dicta
+        earum totam aliquam id, corrupti consectetur delectus corporis sapiente minus. Amet optio inventore ipsa ut!
+    </p>
+@endsection
